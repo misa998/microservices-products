3a9b2fe26792456dd870ef410726a60ee99f07d1
misa998 <milosilic998@gmail.com>
2023-04-08 23:52:47 +0200

    Added api request exception handler for inventory.

---
+++ inventory-service/src/main/java/com/misa/inventoryservice/exception/ApiExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.misa.inventoryservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+        ApiException exception = new ApiException(e.getMessage(), e, HttpStatus.BAD_REQUEST, ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+}
