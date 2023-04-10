db516cd0f8001a22d1df178cfef18fd7ba0d0a78
misa998 <milosilic998@gmail.com>
2023-04-10 23:00:45 +0200

    Added api gateway api authentication and discovery service eureka web auth.

---
+++ discovery-server/src/main/java/com/misa/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.misa.discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((auth) -> auth
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .csrf().ignoringRequestMatchers("/eureka/**")
+                .and().build();
+    }
+}
